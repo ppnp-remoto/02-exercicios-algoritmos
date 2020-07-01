4db76f2d570a9a1091febe1bf97e836c7433cdc9
Gabriel Roger <gabriel.nroger@gmail.com>
2020-07-01 14:52:00 -0300

    Criar exercicio dolar

---
+++ exercicio-dolar.py
@@ -0,0 +1,6 @@
+taxa_dolar = 5.23
+valor_real = float(input("Digite o valor em reais da conversão: "))
+
+resultado = valor_real / taxa_dolar
+
+print("Olá, seu valor de conversão é: ", round(resultado, 2))
